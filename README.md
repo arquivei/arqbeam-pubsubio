56dbd486a31a4c03840b0f4fd5102eb0a34c0b1e
Leonardo Reis <leonardo.reis@arquivei.com.br>
2023-09-05 14:34:55 -0300

    Init project

--- README.md
+++ README.md
@@ -1,2 +1,46 @@
 # arqbeam-sink-pubsub
 An Apache Beam sink for arqbeam-app.
+
+This implementation uses go/pubsub google sdk to publish messages in Pubsub topic.
+
+TL;DR
+
+```go
+package main
+
+import (
+	"context"
+    "github.com/arquivei/arqbeam-sink-pubsub"
+	errorpubsubio "github.com/arquivei/arqbeam-sink-pubsub/error"
+
+	"github.com/apache/beam/sdks/v2/go/pkg/beam"
+	"github.com/apache/beam/sdks/v2/go/pkg/beam/io/textio"
+)
+
+var (
+	pipeline *beam.Pipeline
+)
+
+func getPipeline(ctx context.Context) *beam.Pipeline {
+	if pipeline != nil {
+		return pipeline
+	}
+
+	pipeline := beam.NewPipeline()
+	s := pipeline.Root()
+
+    // Read some files with textio default from apache beam go sdk 
+	readRows := textio.Read(s, config.GCSInputFile)
+
+    // Send each line to pubsub with pubsubio from arqbeam-sink-pubsub
+	pbResult := pubsubio.Publish(s, config.Pubsub.Project, config.Pubsub.Topic, config.Pubsub.BatchSize, readRows)
+
+    // Log if any error happened in publish step
+	errorpubsubio.LogHandler(s, config.Pubsub.BatchSize, pbResult)
+
+	return pipeline
+}
+
+```
+
+Comments, discussions, issues and pull-requests are welcomed.
---
+++ error/log.go
@@ -0,0 +1,55 @@
+package errorpubsubio
+
+import (
+	"context"
+	"sync"
+
+	"cloud.google.com/go/pubsub"
+	"github.com/apache/beam/sdks/v2/go/pkg/beam"
+	"github.com/apache/beam/sdks/v2/go/pkg/beam/log"
+	"github.com/apache/beam/sdks/v2/go/pkg/beam/register"
+)
+
+func init() {
+	register.DoFn2x0[context.Context, *pubsub.PublishResult](&logHandler{})
+}
+
+func LogHandler(s beam.Scope, size int, pbResult beam.PCollection) {
+	s = s.Scope("arqbeam.pubsubio.LogHandler")
+
+	beam.ParDo0(s, &logHandler{
+		Size: size,
+	}, pbResult)
+}
+
+type logHandler struct {
+	Size int
+
+	resultChan chan *pubsub.PublishResult
+	wg         sync.WaitGroup
+}
+
+func (fn *logHandler) Setup(ctx context.Context) {
+	fn.resultChan = make(chan *pubsub.PublishResult, fn.Size*3)
+
+	go func() {
+		for result := range fn.resultChan {
+			id, err := result.Get(ctx)
+			if err != nil {
+				log.Errorf(ctx, "Error in %v: %v", id, err.Error())
+			} else {
+				log.Infof(ctx, "Message processed: %v", id)
+			}
+			fn.wg.Done()
+		}
+	}()
+}
+
+func (fn *logHandler) ProcessElement(ctx context.Context, r *pubsub.PublishResult) {
+	fn.wg.Add(1)
+	fn.resultChan <- r
+}
+
+func (fn *logHandler) FinishBundle() {
+	fn.wg.Wait()
+}
